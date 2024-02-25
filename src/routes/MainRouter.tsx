50fd58d0ff6df9b6d2c6076a5bf146cdeda77f17
Manoranjan <sahoomanoranjan626@gmail.com>
2024-02-25 19:51:23 +0530

    added 404 page for any notfound urls

--- src/routes/MainRouter.tsx
+++ src/routes/MainRouter.tsx
@@ -6,12 +6,14 @@ import {
 } from "react-router-dom";
 import Root from "./Root";
 import HomePage from "../pages/Home";
+import NotFound from "../pages/404";
 
 const MainRouter = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
         <Route index element={<HomePage />} />
+        <Route path="*" element={<NotFound />} />
       </Route>,
     ),
   );
