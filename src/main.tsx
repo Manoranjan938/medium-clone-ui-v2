034415ab96ced41a0cd0e851bf661c23521369cb
Manoranjan <sahoomanoranjan626@gmail.com>
2024-04-12 23:19:05 +0530

    removed react query library

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./shared/tailwind.css";
 import MainRouter from "./routes/MainRouter";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <MainRouter />
-      </QueryClientProvider>
+      <MainRouter />
     </Provider>
   </React.StrictMode>,
 );
